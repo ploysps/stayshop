630f73d9013da989b2dbc6ef6876c1b7ee80a681
ploysps <ploy.cs@gmail.com>
2021-09-05 17:29:30 +0700

    03-setup admin theme & menu items

---
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -0,0 +1,98 @@
+<aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+        <img src="{{asset('backend/assets/img/stayshop.svg')}}"><br>
+        <img class="rounded-circle" src="{{asset('backend/assets/img/profile/female/image_7.png')}}" alt="profile image"> <br>
+    </div>
+    <div class="sidebar-brand sidebar-brand-sm">
+        <a>SS</a>
+    </div>
+    <ul class="sidebar-menu">
+        <li class="menu-header">&nbsp;</li>
+        <li><a class="nav-link" href="blank.html"><i class="fas fa-th-large"></i><span>Dashboard</span></a></li>
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="far fa-image"></i><span>Banner Management</span></a>
+            <ul class="dropdown-menu">
+                <li><a class="nav-link" href="index-0.html">All Banners</a></li>
+                <li><a class="nav-link" href="index.html">Add Banner</a></li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="fas fa-sitemap"></i><span>Category Management</span></a>
+            <ul class="dropdown-menu">
+                <li><a class="nav-link" href="index-0.html">All Departments</a></li>
+                <li><a class="nav-link" href="index.html">Add Departments</a></li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="fas fa-briefcase"></i><span>Products Management</span></a>
+            <ul class="dropdown-menu">
+                <li><a class="nav-link" href="index-0.html">All Departments</a></li>
+                <li><a class="nav-link" href="index.html">Add Departments</a></li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="fas fa-shopping-cart"></i><span>Carts Management</span></a>
+            <ul class="dropdown-menu">
+                <li><a class="nav-link" href="index-0.html">All Departments</a></li>
+                <li><a class="nav-link" href="index.html">Add Departments</a></li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="fas fa-clipboard-list"></i><span>Order Management</span></a>
+            <ul class="dropdown-menu">
+                <li><a class="nav-link" href="index-0.html">All Departments</a></li>
+                <li><a class="nav-link" href="index.html">Add Departments</a></li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="fas fa-sitemap"></i><span>Post Category</span></a>
+            <ul class="dropdown-menu">
+                <li><a class="nav-link" href="index-0.html">All Departments</a></li>
+                <li><a class="nav-link" href="index.html">Add Departments</a></li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="fas fa-tag"></i><span>Post Tag</span></a>
+            <ul class="dropdown-menu">
+                <li><a class="nav-link" href="index-0.html">All Departments</a></li>
+                <li><a class="nav-link" href="index.html">Add Departments</a></li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="far fa-newspaper"></i><span>Post Management</span></a>
+            <ul class="dropdown-menu">
+                <li><a class="nav-link" href="index-0.html">All Departments</a></li>
+                <li><a class="nav-link" href="index.html">Add Departments</a></li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="far fa-star"></i><span>Review Management</span></a>
+            <ul class="dropdown-menu">
+                <li><a class="nav-link" href="index-0.html">All Departments</a></li>
+                <li><a class="nav-link" href="index.html">Add Departments</a></li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="far fa-check-circle"></i><span>Coupon Management</span></a>
+            <ul class="dropdown-menu">
+                <li><a class="nav-link" href="index-0.html">All Departments</a></li>
+                <li><a class="nav-link" href="index.html">Add Departments</a></li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="far fa-user"></i><span>User Management</span></a>
+            <ul class="dropdown-menu">
+                <li><a class="nav-link" href="index-0.html">All Departments</a></li>
+                <li><a class="nav-link" href="index.html">Add Departments</a></li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="far fa-comments"></i><span>Comments Management</span></a>
+            <ul class="dropdown-menu">
+                <li><a class="nav-link" href="index-0.html">All Departments</a></li>
+                <li><a class="nav-link" href="index.html">Add Departments</a></li>
+            </ul>
+        </li>
+        <li><a class="nav-link" href="blank.html"><i class="fas fa-cog"></i> <span>Settings</span></a></li>       
+    </ul>
+</aside>
